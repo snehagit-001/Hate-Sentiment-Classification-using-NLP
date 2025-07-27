77cfd675d6322fc0a29eafc6f90b03fb1bb06927
sneha <snehaathawale17@gmail.com>
2025-07-27 17:06:44 +0530

    requirements added

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Hate-Sentiment-Classification-using-NLP
+
+
+# how to run
+```bash
+python -m venv hate
+```
+
+```bash
+.hate\Scripts\activate
+```
+
+```bash
+pip install -r requirements.txt
+```
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="hate-speech-classification",
+    version="0.01",
+    author="sneha athawale",
+    author_email="snehaathawale17@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
